dc45b9487b18f1c866bc5a0158bcd195cef34d62
Vaibhav Mule <vaibhavmule135@gmail.com>
2018-10-08 20:34:24 +0530

    make dir when craft view

--- masonite/commands/ViewCommand.py
+++ masonite/commands/ViewCommand.py
@@ -15,5 +15,7 @@ class ViewCommand(Command):
         if os.path.isfile('resources/templates/' + name + '.html'):
             self.error('{0} View Exists!'.format(name))
         else:
+            if not os.path.exists(os.path.dirname('resources/templates/{0}.html'.format(name))):
+                os.makedirs(os.path.dirname('resources/templates/{0}.py'.format(name)))
             open('resources/templates/' + name + '.html', 'w+')
             self.info('{0} View Created Successfully!'.format(name))
